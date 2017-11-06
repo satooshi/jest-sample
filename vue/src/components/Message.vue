1c5c741b849e75ff01c9133e982b65774be170fc
Kitamura Satoshi <with.no.parachute@gmail.com>
2017-11-06 18:01:05 +0900

    Add component sample

---
+++ vue/src/components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Message',
+    data() {
+      return {
+        message: 'Hello cat persons!'
+      }
+    }
+  }
+</script>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//import HelloWorld from '@/components/HelloWorld'
+import Message from '@/components/Message'
 
 Vue.use(Router)
 
@@ -9,7 +10,8 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: HelloWorld
+      //component: HelloWorld
+      component: Message
     }
   ]
 })
