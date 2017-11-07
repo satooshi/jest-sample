0443e07cf6cdf5623d6d58e48275e38c108f161f
Kitamura Satoshi <with.no.parachute@gmail.com>
2017-11-07 22:28:09 +0900

    Prevent click event

--- vue/src/components/TodoList.vue
+++ vue/src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <form>
     <div class="list-group">
-      <a v-for="(todoItem, index) in store.state.todos" href="#" class="list-group-item list-group-item-action flex-column align-items-start" v-bind:class="{disabled:todoItem.done}" @click="toggleTodo(index)">
+      <a v-for="(todoItem, index) in store.state.todos" href="#" class="list-group-item list-group-item-action flex-column align-items-start" v-bind:class="{disabled:todoItem.done}" @click.prevent="toggleTodo(index)">
         <div class="d-flex w-100 justify-content-between">
           <h6 class="mb-1">{{todoItem.todo}}</h6>
           <button class="btn btn-link" @click.prevent="deleteTodo(index)">x</button>
