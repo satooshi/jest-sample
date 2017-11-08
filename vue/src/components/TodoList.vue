8aadf8dd26bdbe9dd93096ef0355cd48450fbe39
Kitamura Satoshi <with.no.parachute@gmail.com>
2017-11-08 19:13:48 +0900

    Fix wrong tag name

--- vue/src/components/TodoList.vue
+++ vue/src/components/TodoList.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="list-group">
-    <dev v-for="(todoItem, index) in store.state.todos" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div v-for="(todoItem, index) in store.state.todos" class="list-group-item list-group-item-action flex-column align-items-start">
       <div class="d-flex w-100 justify-content-between">
         <h6 class="mb-1"><input type="checkbox" @click="toggleTodo(index)" v-model="todoItem.done"> {{todoItem.todo}}</h6>
         <button class="btn btn-link" @click.prevent="deleteTodo(index)">x</button>
       </div>
-    </dev>
+    </div>
   </div>
 </template>
 
