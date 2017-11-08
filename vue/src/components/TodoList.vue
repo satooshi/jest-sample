9179f138a3bf4a0090201aa7b6384bbeab61b803
Kitamura Satoshi <with.no.parachute@gmail.com>
2017-11-08 13:45:01 +0900

    Add checkbox to represent done/undone todo

--- vue/src/components/TodoList.vue
+++ vue/src/components/TodoList.vue
@@ -1,14 +1,12 @@
 <template>
-  <form>
-    <div class="list-group">
-      <a v-for="(todoItem, index) in store.state.todos" href="#" class="list-group-item list-group-item-action flex-column align-items-start" v-bind:class="{disabled:todoItem.done}" @click.prevent="toggleTodo(index)">
-        <div class="d-flex w-100 justify-content-between">
-          <h6 class="mb-1">{{todoItem.todo}}</h6>
-          <button class="btn btn-link" @click.prevent="deleteTodo(index)">x</button>
-        </div>
-      </a>
-    </div>
-  </form>
+  <div class="list-group">
+    <dev v-for="(todoItem, index) in store.state.todos" class="list-group-item list-group-item-action flex-column align-items-start">
+      <div class="d-flex w-100 justify-content-between">
+        <h6 class="mb-1"><input type="checkbox" @click="toggleTodo(index)" v-model="todoItem.done"> {{todoItem.todo}}</h6>
+        <button class="btn btn-link" @click.prevent="deleteTodo(index)">x</button>
+      </div>
+    </dev>
+  </div>
 </template>
 
 <script>
