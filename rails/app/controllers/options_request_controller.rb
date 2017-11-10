520eed19bd6ca4c9dcc087d4577ea21ebaf39af7
Kitamura Satoshi <with.no.parachute@gmail.com>
2017-11-10 13:19:35 +0900

    Setup todo rails app

---
+++ rails/app/controllers/options_request_controller.rb
@@ -0,0 +1,18 @@
+class OptionsRequestController < ApplicationController
+  ACCESS_CONTROL_ALLOW_METHODS = %w(GET OPTIONS).freeze
+  ACCESS_CONTROL_ALLOW_HEADERS = %w(Accept Origin Content-Type Authorization Access-Control-Allow-Origin Cache-Control).freeze
+
+  def preflight
+    set_preflight_headers!
+    head :ok
+  end
+
+  private
+
+  def set_preflight_headers!
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Max-Age'] = 3
+    response.headers['Access-Control-Allow-Headers'] = ACCESS_CONTROL_ALLOW_HEADERS.join(',')
+    response.headers['Access-Control-Allow-Methods'] = ACCESS_CONTROL_ALLOW_METHODS.join(',')
+  end
+end
