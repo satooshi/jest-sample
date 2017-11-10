a3ae71cc48f07419e4acc363684d1b24cf26186a
Kitamura Satoshi <with.no.parachute@gmail.com>
2017-11-10 13:20:32 +0900

    Add Todo model, controller

---
+++ rails/app/controllers/todos_controller.rb
@@ -0,0 +1,56 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+  before_action :set_cors
+
+  # GET /todos
+  def index
+    @todos = Todo.all
+
+    render json: @todos
+  end
+
+  # GET /todos/1
+  def show
+    render json: @todo
+  end
+
+  # POST /todos
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      render json: @todo, status: :created, location: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /todos/1
+  def update
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /todos/1
+  def destroy
+    @todo.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def todo_params
+      params.require(:todo).permit(:todo, :done)
+    end
+
+    def set_cors
+      response.headers['Access-Control-Allow-Origin'] = '*'
+    end
+end
